4dc0944ab176686c05ba5e883ba20662fdfbeabd
kuklinv <vitalykuklin@gmail.com>
2019-12-10 12:35:14 +0700

    Feature: 4 files modified, 12 deleted [ref:connect with bootstrap]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,7 @@ import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
-import {FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   imports: [
@@ -18,11 +15,7 @@ import {FooterComponent } from './footer/footer.component';
     ])
   ],
   declarations: [
-    AppComponent,
-    TopBarComponent,
-    ProductListComponent,
-    ProductAlertsComponent,
-    FooterComponent
+    AppComponent
   ],
   bootstrap: [ AppComponent ]
 })
