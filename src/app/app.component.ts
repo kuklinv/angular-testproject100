1b4f08cd793ab1ae67607a1ad47ac06484824207
Vitaly <vitalykuklin@gmail.com>
2019-12-13 08:05:46 +0700

    Feature: add my home project - myTestApp/devcod.pro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {}
+export class AppComponent implements OnInit {
 
+  constructor() {
 
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
+  }
+
+  ngOnInit() {
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,38 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NavigationComponent } from './navigation/navigation.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SkillsComponent } from './pages/skills/skills.component';
+import { CarouselComponent } from './pages/skills/carousel/carousel.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { SandboxComponent } from './pages/sandbox/sandbox.component';
 
 @NgModule({
+  declarations: [
+    AppComponent,
+    NavigationComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    SkillsComponent,
+    CarouselComponent,
+    BlogComponent,
+    PortfolioComponent,
+    ProjectsComponent,
+    SandboxComponent
+  ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule,
-    NgbModule
+    AppRoutingModule
   ],
-  declarations: [
-    AppComponent
-  ],
-  bootstrap: [ AppComponent ]
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
