5d1a773067a3dc971001838e136003e1884910c4
kuklinv <vitalykuklin@gmail.com>
2019-12-18 14:01:53 +0700

    Feature: add @directive and @servise folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import {SkillsComponent} from './pages/skills/skills.component';
 import {PortfolioComponent} from './pages/portfolio/portfolio.component';
 import {ProjectsComponent} from './pages/projects/projects.component';
 import {BlogComponent} from './pages/blog/blog.component';
-import{SandboxComponent} from './pages/sandbox/sandbox.component';
+import {SandboxComponent} from './pages/sandbox/sandbox.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
