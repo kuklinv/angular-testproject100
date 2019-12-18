261161cded116880b40c562649702b38ada9a6c6
kuklinv <vitalykuklin@gmail.com>
2019-12-18 10:06:15 +0700

    Feature: add rputer 3 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './pages/home/home.component';
+import {SkillsComponent} from './pages/skills/skills.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: '', component: SkillsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
