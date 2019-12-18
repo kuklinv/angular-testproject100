d028b6b740198230e40d27c89e457008055ce657
kuklinv <vitalykuklin@gmail.com>
2019-12-18 13:47:04 +0700

    Feature: add  all navigation links with router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,19 @@ import { Routes, RouterModule } from '@angular/router';
 import {AppComponent} from './app.component';
 import {HomeComponent} from './pages/home/home.component';
 import {SkillsComponent} from './pages/skills/skills.component';
-
+import {PortfolioComponent} from './pages/portfolio/portfolio.component';
+import {ProjectsComponent} from './pages/projects/projects.component';
+import {BlogComponent} from './pages/blog/blog.component';
+import{SandboxComponent} from './pages/sandbox/sandbox.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: '', component: SkillsComponent}
+  {path: 'skills', component: SkillsComponent},
+  {path: 'portfolio', component: PortfolioComponent},
+  {path: 'projects' , component: ProjectsComponent},
+  {path: 'blog', component: BlogComponent},
+  {path: 'sandbox', component: SandboxComponent},
+  { path: '**', redirectTo: '/'}
 ];
 
 @NgModule({
